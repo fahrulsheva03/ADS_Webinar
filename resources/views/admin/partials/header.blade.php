574b6501132efe3d2ebd461cd3d2ce69070b5f66
fahrul03 <fahrulshevavanjovie@gmail.com>
2026-01-27 20:46:02 +0800

    update header admin

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <!-- Title -->
-	<title>Ventic - Event Ticketing Bootstrap 5 Admin Template</title>
+	<title>Admin Webinar</title>
 	<!-- Meta -->
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
